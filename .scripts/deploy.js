14026da3f3fb27f641201e89348a72d959ae35a4
Jiangtao <321jiangtao@gmail.com>
2018-01-18 21:24:00 +0800

    add file node env

--- .scripts/deploy.js
+++ .scripts/deploy.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const ghpages = require('gh-pages')
 const now = new Date()
 const y = now.getFullYear()
@@ -8,29 +10,15 @@ const mm = padLeft(now.getMinutes())
 const ss = padLeft(now.getSeconds())
 const date = `${y}-${m}-${d} ${h}:${mm}:${ss}`
 
-
-console.log('uploading')
-;(async function() {
-  try{
-    await new Promise((resolve, reject) => {
-      ghpages.publish(`${process.cwd()}/public`, {
-        remote: 'origin',
-        add: true,
-        branch: 'master',
-        message: `site: update ${date}`,
-        repo: 'https://github.com/jiangtao/jiangtao.github.io.git'
-      }, function(e) {
-        if(e) {
-          reject(e)
-          return
-        }
-        resolve()
-      })
-    })
-    console.log('upload successfully')
-  } catch(e) {
-    console.log(e)
-  }
+ghpages.publish(`${process.cwd()}/public`, {
+  remote: 'origin',
+  add: true,
+  branch: 'master',
+  message: `site: update ${date}`,
+  repo: 'https://github.com/jiangtao/jiangtao.github.io.git'
+}, function(e) {
+  if(e) throw e
+  console.log('uploaded')
 })
 
 function padLeft(n) {
