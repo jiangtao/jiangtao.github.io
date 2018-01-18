d1c30236dd0cb2fc31fcb3da72cde174d97d9397
Jiangtao <321jiangtao@gmail.com>
2018-01-18 21:01:32 +0800

    deploy to master automatically

--- .scripts/deploy.js
+++ .scripts/deploy.js
@@ -10,15 +10,27 @@ const date = `${y}-${m}-${d} ${h}:${mm}:${ss}`
 
 
 console.log('uploading')
-ghpages.publish(`${process.cwd()}/public`, {
-  remote: 'origin',
-  add: true,
-  branch: 'master',
-  message: `site: update ${date}`,
-  repo: 'https://github.com/jiangtao/jiangtao.github.io.git'
-}, function(e) {
-  if(e) throw e
-  console.log('upload successfully')
+;(async function() {
+  try{
+    await new Promise((resolve, reject) => {
+      ghpages.publish(`${process.cwd()}/public`, {
+        remote: 'origin',
+        add: true,
+        branch: 'master',
+        message: `site: update ${date}`,
+        repo: 'https://github.com/jiangtao/jiangtao.github.io.git'
+      }, function(e) {
+        if(e) {
+          reject(e)
+          return
+        }
+        resolve()
+      })
+    })
+    console.log('upload successfully')
+  } catch(e) {
+    console.log(e)
+  }
 })
 
 function padLeft(n) {
